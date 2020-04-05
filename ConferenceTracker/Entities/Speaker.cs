628ffa998104d5b1e12df0a7580948eb4bbedb14
bizon7nt <arekgadek@gmail.com>
2020-04-05 22:29:27 +0200

    3 We should add requirements for lenght of text properties

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -11,16 +11,19 @@ namespace ConferenceTracker.Entities
 
         [Required]
         [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 2)]
         [Display(Name = "First name")]
         public string FirstName { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 2)]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
 
         [Required]
         [DataType(DataType.MultilineText)]
+        [StringLength(500, MinimumLength = 10)]
         public string Description { get; set; }
 
         [DataType(DataType.EmailAddress)]
